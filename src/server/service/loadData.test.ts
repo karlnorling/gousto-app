52e30fadcc0a490560b9a13e5412217e11f07a0f
Karl Norling <karl.whatever@gmail.com>
2019-06-06 14:02:50 +0100

    more tests

--- src/server/service/loadData.test.ts
+++ src/server/service/loadData.test.ts
@@ -1,10 +1,23 @@
 import { loadData }  from './loadData';
 
-const csvFilePath = './src/data/recipe-data.test.csv';
-
 describe('loadData', () => {
   test('should load data successfully', async () => {
-    const recipes = await loadData(csvFilePath);
+    const recipes = await loadData('./src/data/recipe-data.test.csv');
     expect(recipes.length).toBe(1);
   })
+  test('should fail loading data and throw exception - missing file', async () => {
+    try {
+      await loadData('./bad/path.csv');
+    } catch (error) {
+      expect(error).toBe(new Error());
+    }
+  });
+  test('should fail loading data and throw exception - file parsing error', async () => {
+    const csvFilePath = './src/data/recipe-data.test.bad.csv';
+    try {
+      await loadData(csvFilePath);
+    } catch (error) {
+      expect(error).toBe(new Error(`Failed to load data from: ${csvFilePath}. Please check contents of file`));
+    }
+  });
 });
--- src/server/service/loadData.ts
+++ src/server/service/loadData.ts
@@ -5,44 +5,47 @@ export const loadData = async (csvFilePath: string): Promise<Gousto.Recipe[] | v
   console.log(`Attempting to load data from: ${csvFilePath}`);
   try {
     const json = await csv().fromFile(csvFilePath);
-    console.log(`Data loaded: ${JSON.stringify(json)}`);
-    return json.map((data: any) => {
-      const recipe: Gousto.Recipe = {
-        id: data.id,
-        meta: {
-          createdAt: data.created_at,
-          updatedAt: data.updated_at,
-          goustoReference: parseInt(data.gousto_reference, 10)
-        } as Gousto.Meta,
-        boxType: data.box_type,
-        title: data.title,
-        slug: data.slug,
-        shortTitle: data.short_title,
-        marketingDescription: data.marketing_description,
-        bullets: [data.bulletpoint1, data.bulletpoint2, data.bulletpoint3],
-        recipeDietType: data.recipe_diet_type_id,
-        season: data.season,
-        recipeCuisine: data.recipe_cuisine,
-        nutrition: {
-          caloriesKcal: parseInt(data.calories_kcal, 10),
-          proteinGrams: parseInt(data.protein_grams, 10),
-          fatGrams: parseInt(data.fat_grams, 10),
-          carbsGrams: parseInt(data.carbs_grams, 10),
-          originCountry: data.origin_country
-        } as Gousto.Nutrition,
-        ingredients: {
-          inYourBox: data.in_your_box,
-          proteinSource: data.protein_source,
-          base: data.base
-        } as Gousto.Ingredients,
-        cooking: {
-          preparationTimeMinutes: parseInt(data.preparation_time_minutes, 10),
-          shelfLifeDays: parseInt(data.shelf_life_days),
-          equipmentNeeded: data.equipment_needed
-        } as Gousto.Cooking
-      };
-      return recipe;
-    }).filter((recipe: Gousto.Recipe) => !!recipe);
+    if (json && json.length > 0) {
+      console.log(`Data loaded: ${JSON.stringify(json)}`);
+      return json.map((data: any) => {
+        const recipe: Gousto.Recipe = {
+          id: data.id,
+          meta: {
+            createdAt: data.created_at,
+            updatedAt: data.updated_at,
+            goustoReference: parseInt(data.gousto_reference, 10)
+          } as Gousto.Meta,
+          boxType: data.box_type,
+          title: data.title,
+          slug: data.slug,
+          shortTitle: data.short_title,
+          marketingDescription: data.marketing_description,
+          bullets: [data.bulletpoint1, data.bulletpoint2, data.bulletpoint3],
+          recipeDietType: data.recipe_diet_type_id,
+          season: data.season,
+          recipeCuisine: data.recipe_cuisine,
+          nutrition: {
+            caloriesKcal: parseInt(data.calories_kcal, 10),
+            proteinGrams: parseInt(data.protein_grams, 10),
+            fatGrams: parseInt(data.fat_grams, 10),
+            carbsGrams: parseInt(data.carbs_grams, 10),
+            originCountry: data.origin_country
+          } as Gousto.Nutrition,
+          ingredients: {
+            inYourBox: data.in_your_box,
+            proteinSource: data.protein_source,
+            base: data.base
+          } as Gousto.Ingredients,
+          cooking: {
+            preparationTimeMinutes: parseInt(data.preparation_time_minutes, 10),
+            shelfLifeDays: parseInt(data.shelf_life_days),
+            equipmentNeeded: data.equipment_needed
+          } as Gousto.Cooking
+        };
+        return recipe;
+      }).filter((recipe: Gousto.Recipe) => !!recipe);
+    }
+    throw new Error(`Failed to load data from: ${csvFilePath}. Please check contents of file`);
   } catch (error) {
     console.error(`Failed to load data from: ${csvFilePath}`, error);
   }
