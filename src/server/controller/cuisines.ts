c83973c5201c7197eb64500c7a34d75e73433679
Karl Norling <karl.whatever@gmail.com>
2019-06-07 20:42:54 +0100

    Removing trailing comma.

--- src/server/controller/cuisines.ts
+++ src/server/controller/cuisines.ts
@@ -29,7 +29,7 @@ export default class CuisinesController {
         return {
           id: recipe.id,
           title: recipe.title,
-          marketingDescription: recipe.marketingDescription,
+          marketingDescription: recipe.marketingDescription
         }
       });
       const cuisines: Gousto.Cuisine = {
